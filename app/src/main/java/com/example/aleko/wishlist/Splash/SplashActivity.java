beaaebdb3d24284fa9c7ebaecab12fdeb7bfe2ae
Alejandro Hechavarria Gonzalez <alejandrohg2011@gmail.com>
2021-07-15 07:13:54 +0200

    ---> Creating SplashActivity form....

--- app/src/main/java/com/example/aleko/wishlist/Splash/SplashActivity.java
+++ app/src/main/java/com/example/aleko/wishlist/Splash/SplashActivity.java
@@ -1,8 +1,21 @@
 package com.example.aleko.wishlist.Splash;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+import com.example.aleko.wishlist.R;
+
 /**
  * Created by Aleko on 15/07/2021.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_form);
+    }
 }
